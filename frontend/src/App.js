a55eb4b1cf3d7e625f0ffc9d0e58b8bc16014f91
123deysi <deysymancilla123@gmail.com>
2024-05-15 22:06:07 -0400

    navbar y footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+//import Navbar from "./componentes/Navbar";
+
+
+import { Route, Routes } from "react-router-dom";
+//import Analisis from "./Components/pages/Analisis";
+//import Novedades from "./Components/pages/Novedades";
+//import Inicio from "./Components/pages/Inicio";
+
+
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
+
+//import Generalidades from "./Components/pages/Generalidades";
+import Inicio from "./Components/pages/Inicio";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <React.Fragment>
+        <Navbar></Navbar>
+        <Routes>
+          
+        <Route path="/Inicio" element={<Inicio />}></Route>
+        </Routes>
+        <Footer></Footer>
+      </React.Fragment>
+    </main>
+    
   );
 }
 
---
+++ frontend/src/Components/pages/Inicio.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Inicio = () => {
+  return (
+    <div>Inicio aqui va todo lo que sera pagina principal jejejejejje
+      erea capa
+    </div>
+    
+  )
+}
+
+export default Inicio
